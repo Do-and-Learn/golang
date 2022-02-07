4db439b73372d91e252d91f0f202e2e4b0b96bab
Teddy Lee <teddy12090@gmail.com>
2022-02-07 23:26:03 +0800

    html tamplate

---
+++ Chapter06/htmlT.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Entry struct {
+	Number int
+	Double int
+	Square int
+}
+
+func myHandler(data []Entry) func(http.ResponseWriter, *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		const tFile string = "html.gohtml"
+		tmp := template.Must(template.ParseGlob(tFile))
+		tmp.ExecuteTemplate(writer, tFile, data)
+	}
+}
+
+func main() {
+	var data []Entry
+	for i := 20; i < 50; i++ {
+		data = append(data, Entry{i, i * 2, i * i})
+	}
+
+	http.HandleFunc("/", myHandler(data))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Panicln(err)
+	}
+}
