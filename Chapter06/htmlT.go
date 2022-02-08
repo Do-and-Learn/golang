01b9513d3c68fe185540b97ec0cde1ef49b1c4af
Chia-Cheng Lee <teddy@Chia-ChengdeMacBook-Air.local>
2022-02-09 01:59:42 +0800

    learning docker in progress

--- Chapter06/htmlT.go
+++ Chapter06/htmlT.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"log"
 	"net/http"
@@ -21,13 +22,15 @@ func myHandler(data []Entry) func(http.ResponseWriter, *http.Request) {
 }
 
 func main() {
+	_, err := sql.Open("sqlite3", "htmlT.db")
+
 	var data []Entry
 	for i := 20; i < 50; i++ {
 		data = append(data, Entry{i, i * 2, i * i})
 	}
 
 	http.HandleFunc("/", myHandler(data))
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Panicln(err)
 	}
