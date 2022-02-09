aa975000267b288c78a24d8176460919f7fb4a1e
Chia-Cheng Lee <teddy@Chia-ChengdeMacBook-Air.local>
2022-02-09 23:04:37 +0800

    finish html template

--- Chapter06/htmlT.go
+++ Chapter06/htmlT.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Entry struct {
@@ -15,6 +18,7 @@ type Entry struct {
 
 func myHandler(data []Entry) func(http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Host: %s Path: %s\n", request.Host, request.URL.Path)
 		const tFile string = "html.gohtml"
 		tmp := template.Must(template.ParseGlob(tFile))
 		tmp.ExecuteTemplate(writer, tFile, data)
@@ -22,11 +26,31 @@ func myHandler(data []Entry) func(http.ResponseWriter, *http.Request) {
 }
 
 func main() {
-	_, err := sql.Open("sqlite3", "htmlT.db")
+	db, err := sql.Open("sqlite3", "htmlT.db")
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	_, err = db.Exec("DELETE FROM data")
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	stmt, _ := db.Prepare("INSERT INTO data (number, double, square) VALUES (?, ?, ?)")
+	for i := 20; i < 50; i++ {
+		stmt.Exec(i, i*2, i*i)
+	}
+
+	row, err := db.Query("SELECT * FROM data")
+	if err != nil {
+		log.Panicln(err)
+	}
 
 	var data []Entry
-	for i := 20; i < 50; i++ {
-		data = append(data, Entry{i, i * 2, i * i})
+	var n, d, s int
+	for row.Next() {
+		row.Scan(&n, &d, &s)
+		data = append(data, Entry{n, d, s})
 	}
 
 	http.HandleFunc("/", myHandler(data))
